377964b5d09d2c9d670c4812a460e3552a4bea11
RavenR3X <78075608+RavenR3X@users.noreply.github.com>
2021-05-16 22:54:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+menu_list = ["Pizza Margherita","Quick Tomato Pizzas","Four Cheese Pizza","Cheesy Vegetable Pizza","BBQ Chicken Pizza","Chicken Tandoori Pizza"];
+function getMenu()
+{
+    var htmldata
+}
